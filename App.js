5739a6333e62fc577038450915667ee591d21e27
Player One <playerone@Players-MacBook-Pro.local>
2018-01-18 22:11:23 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import Store from './src/Store';
+import { Header, CryptoContainer } from './src/components/index';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      //"The Provider accepts one argument — which is our store. 
+      //The Provider role is to glue React and Redux together."
+      <Provider store={Store}>
+      <View style={styles.container}>
+        <Header />
+        <CryptoContainer />
+      </View>
+      </Provider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
